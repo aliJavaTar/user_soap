1ba91410aac7f362f54d7c7de570fb5b965f182d
A.erfagh <ali77erfagh99@gmail.com>
2022-12-13 12:50:36 +0330

    add unit and integration test

--- src/test/java/com/management/usermanagement/user/application/DefaultUserServiceTest.java
+++ src/test/java/com/management/usermanagement/user/application/DefaultUserServiceTest.java
@@ -11,9 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class DefaultUserServiceTest {
@@ -23,34 +22,53 @@ class DefaultUserServiceTest {
 
     @Test
     void shouldSaveUser() {
-        UserRepository userRepository = mock(UserRepository.class);
 
-        Role role = new Role("admin", null);
         Username username = new Username("ali33");
+        Role role = new Role("admin", null);
+
         UserRequest userRequest = new UserRequest(username, "id", role);
 
-        UserResponse userResponse = new UserResponse(username, "id", role);
+        UserResponse response = userService.save(userRequest);
+        UserResponse result = new UserResponse(username, "id", role);
 
-        UserResponse response = doReturn(userResponse)
-                .when(userRepository.save(userRequest));
-
-        assertThat(response).isEqualTo(userResponse);
+        assertThat(response).isEqualTo(result);
 
     }
 
     @Test
     void shouldSaveUserGiveException_Whit_Invalid_UserName() {
-//        UserRepository watchRepository = mock(UserRepository.class);
+        UserRepository watchRepository = mock(UserRepository.class);
 
         Username username = new Username("ali");
         Role role = new Role("admin", null);
 
         UserRequest userRequest = new UserRequest(username, "id", role);
 
+
         assertThatThrownBy(() -> userService.save(userRequest))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessageStartingWith("user name Not valid");
 
-    }
 
+//        doReturn(response).when(watchRepository).findById(1L);
+
+    }
+    @Test
+    void shouldSaveUser_Unit()
+    {
+
+
+        UserRepository userRepository = mock(UserRepository.class);
+
+        Role role = new Role("admin", null);
+        Username username = new Username("ali33");
+        UserRequest userRequest = new UserRequest(username, "id", role);
+        UserResponse userResponse = new UserResponse(username, "id", role);
+
+
+        when(userRepository.save(userRequest)).thenReturn(userResponse);
+
+
+        assertThat(userResponse).isEqualTo(new UserResponse(username, "id", role));
+    }
 }
