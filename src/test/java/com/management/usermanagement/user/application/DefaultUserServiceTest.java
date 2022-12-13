ec9d9886cde5ca83e5cb9028fbd72e86ba0d799e
A.erfagh <ali77erfagh99@gmail.com>
2022-12-13 12:43:37 +0330

    add unit test whit mock

--- src/test/java/com/management/usermanagement/user/application/DefaultUserServiceTest.java
+++ src/test/java/com/management/usermanagement/user/application/DefaultUserServiceTest.java
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+
 
 @SpringBootTest
 class DefaultUserServiceTest {
@@ -21,19 +23,33 @@ class DefaultUserServiceTest {
 
     @Test
     void shouldSaveUser() {
-        UserRepository watchRepository = mock(UserRepository.class);
+        UserRepository userRepository = mock(UserRepository.class);
 
+        Role role = new Role("admin", null);
         Username username = new Username("ali33");
+        UserRequest userRequest = new UserRequest(username, "id", role);
+
+        UserResponse userResponse = new UserResponse(username, "id", role);
+
+        UserResponse response = doReturn(userResponse)
+                .when(userRepository.save(userRequest));
+
+        assertThat(response).isEqualTo(userResponse);
+
+    }
+
+    @Test
+    void shouldSaveUserGiveException_Whit_Invalid_UserName() {
+//        UserRepository watchRepository = mock(UserRepository.class);
+
+        Username username = new Username("ali");
         Role role = new Role("admin", null);
 
         UserRequest userRequest = new UserRequest(username, "id", role);
 
-        UserResponse response = userService.save(userRequest);
-        UserResponse result = new UserResponse(username, "id", role);
-
-        assertThat(response).isEqualTo(result);
-
-        doReturn(response).when(watchRepository).findById(1L);
+        assertThatThrownBy(() -> userService.save(userRequest))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageStartingWith("user name Not valid");
 
     }
 
